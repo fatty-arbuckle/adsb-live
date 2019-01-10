29db86ec43eaf6c5e89f83687086170e72c7b9b9
Kurt Severance <Kurt.Severance@rsa.com>
2019-01-10 11:22:13 -0500

    client unit test cleanup

--- apps/dump_1090_client/lib/dump_1090_client/network/client.ex
+++ apps/dump_1090_client/lib/dump_1090_client/network/client.ex
@@ -1,37 +1,6 @@
 defmodule Dump1090Client.Network.Client do
-
   use GenServer
-
   require Logger
-
-  @max_retries 10
-  @retry_interval 1000
-
-  defmodule DefaultCallbacks do
-    require Logger
-
-    def on_connect(state) do
-      Logger.debug("tcp connect to #{state.host}:#{state.port}", ansi_color: :yellow)
-    end
-
-    def on_disconnect(state) do
-      Logger.debug("tcp disconnect from #{state.host}:#{state.port}", ansi_color: :yellow)
-    end
-
-    def on_failure(state) do
-      Logger.debug("tcp failure from #{state.host}:#{state.port}. Max retries exceeded.", ansi_color: :yellow)
-    end
-  end
-
-  defmodule State do
-    defstruct host: "nope",
-              port: 1234,
-              failure_count: 0,
-              on_connect: &DefaultCallbacks.on_connect/1,
-              on_disconnect: &DefaultCallbacks.on_disconnect/1,
-              on_failure: &DefaultCallbacks.on_failure/1
-
-  end
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
@@ -42,7 +11,7 @@ defmodule Dump1090Client.Network.Client do
     connect(state)
   end
 
-  def connect(state) do
+  defp connect(state) do
     case :gen_tcp.connect(state.host, state.port, []) do
       {:ok, _socket} ->
         state.on_connect.(state)
@@ -50,7 +19,7 @@ defmodule Dump1090Client.Network.Client do
       {:error, _reason} ->
         new_state = %{state | failure_count: 1}
         new_state.on_disconnect.(new_state)
-        {:ok, new_state, @retry_interval}
+        {:ok, new_state, state.retry_interval}
     end
   end
 
@@ -65,8 +34,8 @@ defmodule Dump1090Client.Network.Client do
     {:noreply, state}
   end
 
-  def handle_info(:timeout, state = %State{failure_count: failure_count}) do
-    if failure_count <= @max_retries do
+  def handle_info(:timeout, state = %{failure_count: failure_count}) do
+    if failure_count <= state.max_retries do
       case :gen_tcp.connect(state.host, state.port, []) do
         {:ok, _socket} ->
           new_state = %{state | failure_count: 0}
@@ -75,32 +44,77 @@ defmodule Dump1090Client.Network.Client do
         {:error, _reason} ->
           new_state = %{state | failure_count: failure_count + 1}
           new_state.on_disconnect.(new_state)
-          :timer.sleep(60 * 1000)
-          {:noreply, new_state, @retry_interval}
+          :timer.sleep(state.retry_delay)
+          {:noreply, new_state, state.retry_interval}
       end
     else
-      state.on_failure.(state)
+      state.on_retries_exceeded.(state)
       {:stop, :max_retry_exceeded, state}
     end
   end
 
-  def handle_info({:tcp_closed, _socket}, state) do
-    case :gen_tcp.connect(state.host, state.port, []) do
-      {:ok, _socket} ->
-        new_state = %{state | failure_count: 0}
-        new_state.on_connect.(new_state)
-        {:noreply, new_state}
-      {:error, _reason} ->
-        new_state = %{state | failure_count: 1}
-        new_state.on_disconnect.(new_state)
-        {:noreply, new_state, @retry_interval}
-    end
-  end
+  # not used?
+  # def handle_info({:tcp_closed, _socket}, state) do
+  #   case :gen_tcp.connect(state.host, state.port, []) do
+  #     {:ok, _socket} ->
+  #       new_state = %{state | failure_count: 0}
+  #       new_state.on_connect.(new_state)
+  #       {:noreply, new_state}
+  #     {:error, _reason} ->
+  #       new_state = %{state | failure_count: 1}
+  #       new_state.on_disconnect.(new_state)
+  #       {:noreply, new_state, state.retry_interval}
+  #   end
+  # end
 
   defp opts_to_initial_state(opts) do
-    host = Keyword.get(opts, :host, "localhost") |> String.to_charlist
-    port = Keyword.fetch!(opts, :port)
-    %State{host: host, port: port}
+    state = %{
+      host: "localhost",
+      port: 30003,
+      max_retries: 10,
+      retry_interval: 1000,
+      retry_delay: 60000,
+      failure_count: 0,
+      on_connect: fn state ->
+        Logger.debug("tcp connect to #{state.host}:#{state.port}", ansi_color: :light_blue)
+      end,
+      on_disconnect: fn state ->
+        Logger.debug("tcp disconnect from #{state.host}:#{state.port}", ansi_color: :light_blue)
+      end,
+      on_retries_exceeded: fn state ->
+        Logger.info("Max retries exceeded for #{state.host}:#{state.port}.")
+      end,
+    }
+
+    state = if Keyword.has_key?(opts, :host) do
+      %{ state | host: Keyword.get(opts, :host, "localhost") |> String.to_charlist }
+    else
+      state
+    end
+
+    state = if Keyword.has_key?(opts, :port) do
+      %{ state | port: Keyword.fetch!(opts, :port) }
+    else
+      state
+    end
+
+    state = if Keyword.has_key?(opts, :max_retries) do
+      %{ state | max_retries: Keyword.fetch!(opts, :max_retries) }
+    else
+      state
+    end
+
+    state = if Keyword.has_key?(opts, :retry_interval) do
+      %{ state | retry_interval: Keyword.fetch!(opts, :retry_interval) }
+    else
+      state
+    end
+
+    state = if Keyword.has_key?(opts, :retry_delay) do
+      %{ state | retry_delay: Keyword.fetch!(opts, :retry_delay) }
+    else
+      state
+    end
   end
 
 end
